7b562240606ccad4f7dc1acbe67a5ff406ab7a8a
Saurabh Mathur <saurabhmathur96@gmail.com>
2014-11-06 20:27:12 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@
 		$scope.projects = [{title: "twitter-analysis", description: "Plots the frequency of hashtags related to a topic on twitter.", link_to_code: "https://github.com/saurabhmathur96/twitter-analysis"},
 		{title: "CBSE-Results-Scraper", description: "Gets the results of a range of roll numbers from the CBSE website.", link_to_code: "https://github.com/saurabhmathur96/CBSE-Results-Scraper"},
 		{title: "PhoneBook", description: "A console application for storage, retrieval and modification of telephone records using C++.", link_to_code: "https://github.com/saurabhmathur96/PhoneBook"},
-		{title: "saurabhmathur96.github.io", description: "This website, designed with Angular.js.", link_to_code: "https://github.com/saurabhmathur96/saurabhmathur96.github.io"}
+		{title: "saurabhmathur96.github.io", description: "This website, designed with Angular.js.", link_to_code: "https://github.com/saurabhmathur96/saurabhmathur96.github.io"},
 		{title: "Sieve-of-Eratosthenes", description: "A prime number generator made in C.", link_to_code: "https://github.com/saurabhmathur96/Sieve-of-Eratosthenes"}
 		]
 	});
